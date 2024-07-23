1c39ad4744ea247b6b1091c97a89a627a53a5d2f
Lee <wpdud1221@naver.com>
2024-07-23 22:01:59 +0900

    폴더 기본구조 , test commit

---
+++ src/main/java/com/stu/auction/api/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.stu.auction.api.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test1(){
+        return "테스트 완료";
+    }
+
+}
