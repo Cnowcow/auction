f5d40bc41cf35515f90044788e6965827737f6a3
nowcow <hhjnn92@icloud.com>
2024-07-26 14:35:26 +0900

    타임리프 의존성 주입, 컨트롤러 테스트

--- src/main/java/com/stu/auction/api/test/controller/TestController.java
+++ src/main/java/com/stu/auction/api/test/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.stu.auction.api.test.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/test")
@@ -12,5 +14,20 @@ public class TestController {
     public String test1(){
         return "테스트 완료";
     }
+
+
+
+    //테스트 해보는 것
+    @GetMapping("/test")
+    public ModelAndView test(HttpSession session) {
+
+        System.out.println("session = " + session);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("views/test"); // "templates/views/test.html" 파일을 뷰로 사용
+
+        return modelAndView;
+    }
+
 
 }
