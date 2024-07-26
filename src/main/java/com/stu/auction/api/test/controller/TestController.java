fbc08cff018221b38299938c790ab17bd1dad9bd
nowcow <hhjnn92@icloud.com>
2024-07-26 17:11:38 +0900

    테스트용 entity 추가

--- src/main/java/com/stu/auction/api/test/controller/TestController.java
+++ src/main/java/com/stu/auction/api/test/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.stu.auction.api.test.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +12,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping
     public String test1(){
@@ -26,6 +32,7 @@ public class TestController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("views/test"); // "templates/views/test.html" 파일을 뷰로 사용
 
+        logger.info("reservation test");
         return modelAndView;
     }
 
